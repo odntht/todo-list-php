7ab59868aa48c6ff2cc37aae308eacf92ff87a27
Fernando Cavalcante <odntht@gmail.com>
2025-01-30 19:27:33 -0300

    parte 3 - CRUD com PHP

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $('.edit-button').on('click', function() {
+    $('.edit-button').on('click', function () {
         var $task = $(this).closest('.task')
         $task.find('.progress').addClass('hidden')
         $task.find('.task-description').addClass('hidden')
@@ -7,11 +7,36 @@ $(document).ready(function () {
         $task.find('.edit-task').removeClass('hidden')
     })
 
-    $('.progress').on('click', function() {
-        if($(this).is(':checked')) {
+    $('.progress').on('click', function () {
+        if ($(this).is(':checked')) {
             $(this).addClass('done')
         } else {
             $(this).removeClass('done')
         }
+    })
+
+    $('.progress').on('change', function () {
+        const id = $(this).data('task-id')
+        const completed = $(this).is(':checked') ? 'true' : 'false'
+        $.ajax({
+            url: '../../actions/update-progress.php',
+            method: 'POST',
+            data: {
+                id: id,
+                completed: completed
+            },
+            dataType: 'json',
+            success: function (response) {
+                console.log(response)
+                if (response.success) {
+
+                } else {
+                    alert('Erro ao editar a tarefa')
+                }
+            },
+            error: function () {
+                alert('Erro ao editar a tarefa')
+            }
+        });
     })
 })
